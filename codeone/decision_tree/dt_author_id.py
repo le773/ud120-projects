dd2274d5b060bcaaee0aa5bb82be0fc690b1438c
hostfox <le773@users.noreply.github.com>
2018-03-09 17:59:55 +0800

    Update dt_author_id.py

--- codeone/decision_tree/dt_author_id.py
+++ codeone/decision_tree/dt_author_id.py
@@ -9,7 +9,8 @@
 
 import sys
 from time import time
-sys.path.append("../tools/")
+import os
+sys.path.append(os.getcwd() + "\\codeone\\tools")
 from email_preprocess import preprocess
 
 
@@ -18,11 +19,27 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
-
-
+print("features_train:",len(features_train))
+print("features_test:",len(features_test))
+print("labels_train:",len(labels_train))
+print("labels_test:",len(labels_test))
 #########################################################
 ### your code goes here ###
 
+from sklearn import tree
+clf = tree.DecisionTreeClassifier()
 
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
 #########################################################
+t1 = time()
+labels_predict = clf.predict(features_test)
+print "predict time:", round(time()-t1, 3), "s"
+
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+
+print "accuracy_score:", accuracy_score(labels_predict, labels_test)
+print "precision_score:", precision_score(labels_predict, labels_test)
+print "recall_score:", recall_score(labels_predict, labels_test)
+print "f1_score:", f1_score(labels_predict, labels_test)
